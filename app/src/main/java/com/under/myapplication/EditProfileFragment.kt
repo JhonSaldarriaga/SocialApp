3a20dca1c68f2a0e83615117b0c5695ac16ad8ce
JhonS <jhonsaldarriagalopez@gmail.com>
2022-04-10 01:53:24 -0500

    fragment_edit_profile V1

---
+++ app/src/main/java/com/under/myapplication/EditProfileFragment.kt
@@ -0,0 +1,42 @@
+package com.under.myapplication
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.DialogFragment
+import com.under.myapplication.databinding.FragmentEditProfileBinding
+
+class EditProfileFragment : DialogFragment() {
+
+    private var _binding: FragmentEditProfileBinding? = null
+    private val binding get() = _binding!!
+
+    //ACOMODA LA VISTA
+    override fun onStart() {
+        super.onStart()
+        dialog?.window?.setLayout(
+            ViewGroup.LayoutParams.MATCH_PARENT,
+            ViewGroup.LayoutParams.WRAP_CONTENT)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        _binding = FragmentEditProfileBinding.inflate(inflater,container,false)
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+    companion object {
+        @JvmStatic
+        fun newInstance() = EditProfileFragment()
+    }
+}
