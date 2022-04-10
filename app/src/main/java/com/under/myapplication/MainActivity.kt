35d65228b4f063a89ac309e0097bd94738d255ae
JhonS <jhonsaldarriagalopez@gmail.com>
2022-04-09 23:24:55 -0500

    selector of menuItems have been fixed

--- app/src/main/java/com/under/myapplication/MainActivity.kt
+++ app/src/main/java/com/under/myapplication/MainActivity.kt
@@ -1,11 +1,19 @@
 package com.under.myapplication
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.under.myapplication.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private val binding: ActivityMainBinding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+        startActivity(Intent(this, UserActivity::class.java))
     }
 }
