228325137805ca095e8118cf7c93fec02a50bac6
JhonS <jhonsaldarriagalopez@gmail.com>
2022-04-10 20:17:06 -0500

    spinner has been implemented

--- app/src/main/java/com/under/myapplication/NewPostFragment.kt
+++ app/src/main/java/com/under/myapplication/NewPostFragment.kt
@@ -1,11 +1,15 @@
 package com.under.myapplication
 
+import android.graphics.BitmapFactory
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
 import com.under.myapplication.databinding.FragmentNewPostBinding
+import com.under.myapplication.model.DataBase
 
 class NewPostFragment : Fragment() {
 
@@ -18,7 +22,27 @@ class NewPostFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         _binding = FragmentNewPostBinding.inflate(inflater,container,false)
+        updateInfo()
+        val adapter = ArrayAdapter(requireContext(), android.R.layout.simple_spinner_item, resources.getStringArray(R.array.new_post_spinner_optiones))
+        binding.spinnerLocality.adapter = adapter
+        adapter.setDropDownViewResource(
+            android.R.layout.simple_spinner_dropdown_item
+        )
+
         return binding.root
+    }
+
+    fun updateInfo(){
+        var actualUser = DataBase.getUserByID(DataBase.getSession()!!.userID)
+        var profilePic = actualUser?.getImageProfilePaht()
+        val profileName = actualUser?.getName()
+
+        if(profilePic!=""){
+            val bitmap = BitmapFactory.decodeFile(profilePic)
+            binding.newPostFragmentProfilePicIV.setImageBitmap(bitmap)
+        }else{ binding.newPostFragmentProfilePicIV.setImageResource(R.drawable.default_profile_pic) }
+
+        binding.newPostFragmentProfileNameTV.text = profileName
     }
 
     override fun onDestroyView() {
